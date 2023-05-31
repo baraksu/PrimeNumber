418b1954cfeba431631fdc9c5f8d15f2df8b8761
amikam123 <132656634+amikam123@users.noreply.github.com>
2023-05-31 22:03:10 +0300

    Update asrmbley 9.6.asm

--- asrmbley 9.6.asm
+++ asrmbley 9.6.asm
@@ -1,69 +1,141 @@
 .MODEL small
 .STACK 100h
 .DATA
-    msg1 db 13,10,'pls enter your first number: $'
-    msg2 db 13,10,'pls enter your second number: $'
-    var1 dw ?
-    var2 dw ?
-    max dw ?    
+    num db ? 
+    msg1 db 13,10,'Enter your number: $'   
+    msg2 db 13,10,'Prime number$'
+    msg3 db 13,10,'Not a prime number$' 
+    msg4 db 13,10,'The next prime number is: $' 
+    gb dw ? ;I used this variable because I didn't have enough registers
+    sum db ?
+    temp db ?
+    temp2 db ?;I used another one because otherwise it would have changed in the procedure I called from the inside 
 .CODE
-    proc high
-        push bp
-        mov bp,sp 
-        
-        mov ax,[bp+6]
-        cmp ax,[bp+8]
-        ja first
-        
-        cmp ax,[bp+8]
-        jb second
-        
-        first:
-            mov bx,[bp+4]
-            mov cx,[bp+6]
-            mov [bx],cx
-            jmp cnt
-        second:
-            mov bx,[bp+4]
-            mov cx,[bp+8]
-            mov [bx],cx
-        cnt:
-        pop bp
-        ret 6
-    endp high 
-    
 start:
     mov ax,@data
     mov ds,ax
-    xor ax,ax
+    xor ax,ax 
     
     lea dx,msg1
-    mov ah,09
-    int 21h
+    push dx
+    call print
     
     mov ah,01
-    int 21h
-    xor ah,ah
+    int 21h 
     sub al,48
-    mov var1,ax
+    xor ah,ah 
+    push ax
+    call checkPrime
     
-    lea dx,msg2
-    mov ah,09
-    int 21h
+    push dx
+    call priAns
     
-    mov ah,01
-    int 21h
-    xor ah,ah
-    sub al,48
-    mov var2,ax
+    xor ax,ax
+    mov al,num
+    push ax
+    call nextPrime
     
-    push var1
-    push var2
-    push offset max
+    jmp finalExit
     
-    call high
-    jmp exit         
-exit:
-end start
+    
+;A procedure that prints what I enter to it
+proc print
+        pop cx
+        pop dx
+        mov ah,09
+        int 21h
+        push cx
+        ret
+endp print    
+;A procedure that checks if the number I entered is a prime number    
+proc checkPrime
+    pop bx
+    pop ax
+    mov temp,al
+    mov num,al
+    mov cl,num
+    xor ch,ch
+    
+    loopPrime:
+        xor ah,ah 
+        mov al,num
+        div temp
+        cmp ah,0
+        je zero  
+        jmp cnt
+        
+        zero:
+            add sum,1 
+        cnt:
+            sub temp,1
+        loop loopPrime
+    
+    cmp sum,2
+    je prime
+    jmp cont
+    
+    prime:
+        mov dx,0
+        jmp exit
+    cont:
+        mov dx,1
+    exit: 
+        mov sum,0
+        push bx
+        ret
+endp checkPrime
+;A procedure that prints whether the number is prime or not
+proc priAns
+    pop gb 
+    pop cx
+    cmp cx,0
+    je priPrime
+    
+    lea dx,msg3
+    push dx
+    call print
+    jmp exit2
+    
+    priPrime:
+        lea dx,msg2
+        push dx
+        call print
+    exit2:
+        push gb
+        ret
+endp priAns
+;A procedure that finds the next prime number
+proc nextPrime
+    pop gb
+    pop ax 
+    loopnext:
+        add ax,1
+        push ax
+        call checkPrime
+        cmp dx,0
+        je nextPrime2
+        jmp cntLoop
+        
+        nextPrime2:
+            mov temp2,al
+            lea dx,msg4
+            push dx
+            call print
+            add temp2,48
+            mov dl,temp2
+            mov ah,02
+            int 21h
+            jmp exit3
+        cntLoop:
+            loop loopnext
+    exit3:
+        push gb
+        ret
+endp nextPrime
+    
+    finalExit:
+        mov ah,4ch
+        int 21h   
+END start
 
 
